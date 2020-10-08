c93dc1fc1b568c6ede83f1e335efbf48ae3cc286
Galahad <derwin499@gmail.com>
2020-10-08 17:10:35 -0400

    now guarantees each selected form of character

--- script.js
+++ script.js
@@ -1,111 +1,209 @@
-var spcChars = ['`','~','!','@','#','$','^','&','*','(',')','_','-','=','+','|','{','}','[',']',':',';',',','<','.','>','/','?'];
+let spcChars = [
+  "`",
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "=",
+  "+",
+  "|",
+  "{",
+  "}",
+  "[",
+  "]",
+  ":",
+  ";",
+  ",",
+  "<",
+  ".",
+  ">",
+  "/",
+  "?",
+];
 
-var lwrCase = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
+let lwrCase = [
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+];
 
-var upprCase = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
+let upprCase = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+];
 
-var number = ['1','2','3','4','5','6','7','8','9','0'];
-
-
+let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 function askRequirements() {
-  
-  var passLength = parseInt(
-    prompt('How many characters will the password be?'));
+  let passLength = parseInt(
+    prompt("How many characters will the password be?")
+  );
 
-  if (isNaN(passLength) === true || passLength > 128 || passLength < 8){
-    alert('May only choose a number between 8 and 128.');
-    return;
+  if (isNaN(passLength) === true || passLength > 128 || passLength < 8) {
+    alert("May only choose a number between 8 and 128.");
 
-  };
-  
-  var hasSpcChars = confirm('Add special characters?');
+    return askRequirements();
+  }
 
-  var hasLwrCase = confirm('Add lower case characters?');
+  let hasSpcChars = confirm("Add special characters?");
 
-  var hasUpprCase = confirm('Add upper case characters?');
+  let hasLwrCase = confirm("Add lower case characters?");
 
-  var hasNumbers = confirm('Add numbers?');
+  let hasUpprCase = confirm("Add upper case characters?");
 
-  if (hasSpcChars||hasLwrCase||hasUpprCase||hasNumbers){
-   
-    var requirements = {
+  let hasNumbers = confirm("Add numbers?");
+
+  if (hasSpcChars || hasLwrCase || hasUpprCase || hasNumbers) {
+    let requirements = {
       passLength: passLength,
       hasSpcChars: hasSpcChars,
       hasLwrCase: hasLwrCase,
       hasUpprCase: hasUpprCase,
       hasNumbers: hasNumbers,
     };
-    return requirements;
 
+    return requirements;
   } else {
-    alert('You must choose at least one character type.')
-  };
-};
+    alert("You must choose at least one character type.");
+    return askRequirements();
+  }
+}
 
 function randomizer(obj) {
-  var indx = Math.floor(Math.random() * obj.length);
-  var rndItem = obj[indx];
+  let indx = Math.floor(Math.random() * obj.length);
+
+  let rndItem = obj[indx];
 
   return rndItem;
-};
+}
 
+function generatePassword() {
+  //Asks user for password requirements letar passReq = askRequirements();
+  let passReq = askRequirements();
+  //Creates variable arrays
+  let passPool = [];
 
+  let passDone = [];
 
- function generatePassword() {
+  let count = 0;
 
-    var passReq = askRequirements();
+  // Grabing random pool of characters to guarantee requirements are met
+  if (passReq.hasSpcChars === true) {
+    count++;
+    for (let i = 0; i < passReq.passLength; i++) {
+      passPool.push(randomizer(spcChars));
+    }
 
-    var passChars = [];
+    passDone.push(randomizer(spcChars));
+  }
 
-    var passDone = [];
+  //
+  if (passReq.hasLwrCase) {
+    count++;
+    for (let i = 0; i < passReq.passLength; i++) {
+      passPool.push(randomizer(lwrCase));
+    }
 
-    if (passReq.hasSpcChars) {
-      var passChars = passChars.concat(spcChars);
-    };
+    passDone.push(randomizer(lwrCase));
+  }
 
-    if (passReq.hasLwrCase) {
-      var passChars = passChars.concat(lwrCase);
-    };
+  //
+  if (passReq.hasUpprCase) {
+    count++;
+    for (let i = 0; i < passReq.passLength; i++) {
+      passPool.push(randomizer(upprCase));
+    }
 
-    if (passReq.hasUpprCase) {
-      var passChars = passChars.concat(upprCase);
-    };
+    passDone.push(randomizer(upprCase));
+  }
 
-    if (passReq.hasNumbers) {
-      var passChars = passChars.concat(number);
-    };
+  //
+  if (passReq.hasNumbers) {
+    count++;
+    for (let i = 0; i < passReq.passLength; i++) {
+      passPool.push(randomizer(number));
+    }
 
+    passDone.push(randomizer(number));
+  }
 
-    for (var i = 0; i < passReq.passLength; i++) {
-      var passChar = randomizer(passChars);
-  
-      passDone.push(passChar);
-    };
+  //Adding characters to meet required length
+  console.log(count);
 
-    return passDone.join('');
+  for (let i = 0; i < passReq.passLength - count; i++) {
+    let passChar = randomizer(passPool);
 
- };
+    passDone.push(passChar);
+  }
 
-
-
-
-
-
-
-
-
+  //Makes it all a single prinatble string
+  return passDone.join("");
+}
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
