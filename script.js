e5f894cfe17b2841f10e5d88e19e0cfaa7f21b21
Galahad <derwin499@gmail.com>
2020-10-10 09:48:47 -0400

    added polish and notes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let spcChars = [
+//Define possible character arrays
+var spcChars = [
   "`",
   "~",
   "!",
@@ -29,7 +30,7 @@ let spcChars = [
   "?",
 ];
 
-let lwrCase = [
+var lwrCase = [
   "q",
   "w",
   "e",
@@ -58,7 +59,7 @@ let lwrCase = [
   "m",
 ];
 
-let upprCase = [
+var upprCase = [
   "Q",
   "W",
   "E",
@@ -87,95 +88,97 @@ let upprCase = [
   "M",
 ];
 
-let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
+//Function to receive user input for password requirements
 function askRequirements() {
-  let passLength = parseInt(
+  var passLength = parseInt(
     prompt("How many characters will the password be?")
   );
 
   if (isNaN(passLength) === true || passLength > 128 || passLength < 8) {
     alert("May only choose a number between 8 and 128.");
-
-    return askRequirements();
+    return;
   }
 
-  let hasSpcChars = confirm("Add special characters?");
+  var hasSpcChars = confirm("Add special characters?");
 
-  let hasLwrCase = confirm("Add lower case characters?");
+  var hasLwrCase = confirm("Add lower case characters?");
 
-  let hasUpprCase = confirm("Add upper case characters?");
+  var hasUpprCase = confirm("Add upper case characters?");
 
-  let hasNumbers = confirm("Add numbers?");
+  var hasNumbers = confirm("Add numbers?");
 
+  //Store requirement values in an array
   if (hasSpcChars || hasLwrCase || hasUpprCase || hasNumbers) {
-    let requirements = {
+    var requirements = {
       passLength: passLength,
       hasSpcChars: hasSpcChars,
       hasLwrCase: hasLwrCase,
       hasUpprCase: hasUpprCase,
       hasNumbers: hasNumbers,
     };
-
     return requirements;
+
+    //Ends if no character type selected
   } else {
     alert("You must choose at least one character type.");
-    return askRequirements();
   }
 }
 
+//Randomizer function
 function randomizer(obj) {
-  let indx = Math.floor(Math.random() * obj.length);
-
-  let rndItem = obj[indx];
+  var indx = Math.floor(Math.random() * obj.length);
+  var rndItem = obj[indx];
 
   return rndItem;
 }
 
 function generatePassword() {
-  //Asks user for password requirements letar passReq = askRequirements();
-  let passReq = askRequirements();
+  //Asks user for password requirements
+  var passReq = askRequirements();
+
   //Creates variable arrays
-  let passPool = [];
+  var passPool = [];
 
-  let passDone = [];
+  var passDone = [];
 
-  let count = 0;
+  var count = 0;
 
   // Grabing random pool of characters to guarantee requirements are met
-  if (passReq.hasSpcChars === true) {
+  if (passReq.hasSpcChars) {
     count++;
-    for (let i = 0; i < passReq.passLength; i++) {
+    for (var i = 0; i < passReq.passLength; i++) {
       passPool.push(randomizer(spcChars));
     }
 
     passDone.push(randomizer(spcChars));
   }
 
-  //
+  //Same
   if (passReq.hasLwrCase) {
     count++;
-    for (let i = 0; i < passReq.passLength; i++) {
+    for (var i = 0; i < passReq.passLength; i++) {
       passPool.push(randomizer(lwrCase));
     }
 
     passDone.push(randomizer(lwrCase));
   }
 
-  //
+  //Same
   if (passReq.hasUpprCase) {
     count++;
-    for (let i = 0; i < passReq.passLength; i++) {
+    for (var i = 0; i < passReq.passLength; i++) {
       passPool.push(randomizer(upprCase));
     }
 
     passDone.push(randomizer(upprCase));
   }
 
-  //
+  //Same
   if (passReq.hasNumbers) {
     count++;
-    for (let i = 0; i < passReq.passLength; i++) {
+    for (var i = 0; i < passReq.passLength; i++) {
       passPool.push(randomizer(number));
     }
 
@@ -183,10 +186,8 @@ function generatePassword() {
   }
 
   //Adding characters to meet required length
-  console.log(count);
-
-  for (let i = 0; i < passReq.passLength - count; i++) {
-    let passChar = randomizer(passPool);
+  for (var i = 0; i < passReq.passLength - count; i++) {
+    var passChar = randomizer(passPool);
 
     passDone.push(passChar);
   }
@@ -196,12 +197,12 @@ function generatePassword() {
 }
 
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
